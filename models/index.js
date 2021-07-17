f26a47fc645639a409d1b82ab92a05e19b9f51e9
forrestpark <pjwoo3@gmail.com>
2021-07-17 22:22:09 +0900

    now importing .env file in /models/index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
