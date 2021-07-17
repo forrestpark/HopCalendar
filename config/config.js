1670cb28cf3ca145a121d48d3147303757ded486
forrestpark <pjwoo3@gmail.com>
2021-07-17 22:14:50 +0900

    added db credentials

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const env = process.env;
 
 const development = {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "jangwoopark",
+    "password": "pc112400pjw3",
+    "database": "hopcalendar2.0",
+    "host": "localhost",
+    "dialect": "postgres"
 }
 
 const test = {
