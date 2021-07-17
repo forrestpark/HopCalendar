9048952db9d63d1a2b432f4e3aa6485cd19f745d
forrestpark <pjwoo3@gmail.com>
2021-07-17 22:41:15 +0900

    added nodemon

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || 3000;
 
 console.log("process.env.PORT: ", process.env.PORT)
 
+app.get('/', (_, res) => {
+    res.send("rebuilding HopCalendar")
+});
+
 app.listen( port, function() {
     console.log('Express listening on port', port);
 });
