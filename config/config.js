b4a5d18149fdc7b991ab5acf4f20d585b9211207
forrestpark <pjwoo3@gmail.com>
2021-07-17 22:06:31 +0900

    first commit: rebuilding hopcalendar

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const env = process.env;
+
+const development = {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+}
+
+const test = {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+
+const production = {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
---
+++ migrations/20210717130319-create-course.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      admins: {
+        type: Sequelize.STRING
+      },
+      instructor: {
+        type: Sequelize.STRING
+      },
+      classNumber: {
+        type: Sequelize.STRING
+      },
+      tasks: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Courses');
+  }
+};
