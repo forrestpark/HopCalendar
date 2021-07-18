cc2940b6d5b0bc357c667aba27f53c846fe81ecc
forrestpark <pjwoo3@gmail.com>
2021-07-18 17:16:02 +0900

    changed base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:3000'
-// const BASE_URL = 'https://hopcalendar.herokuapp.com'
+// const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://hopcalendar.herokuapp.com'
 export { BASE_URL }
