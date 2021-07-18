0c92ce59289c1183c4d551243c25d7c895603a42
forrestpark <pjwoo3@gmail.com>
2021-07-18 16:22:30 +0900

    changed db migration settings

--- migrations/20210717130319-create-course.js
+++ migrations/20210717130319-create-course.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Courses', {
+    await queryInterface.createTable('courses', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Courses');
+    await queryInterface.dropTable('courses');
   }
 };
